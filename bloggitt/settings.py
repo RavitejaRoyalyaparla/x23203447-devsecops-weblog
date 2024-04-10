3209bfb21fb20df1294bb6dd97f28c05929f657e
RavitejaRoyalyaparla <x23203447@student.ncirl.ie>
2024-04-10 11:07:00 +0100

    changes

--- bloggitt/settings.py
+++ bloggitt/settings.py
@@ -137,11 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATICFILES_DIRS =  [os.path.join(BASE_DIR, 'static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
